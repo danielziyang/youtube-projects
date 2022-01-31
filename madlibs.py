7fd97d4c0285967f8875aef81e3d340666f51cf7
Daniel Zi Yang <dziyang95@gmail.com>
2022-01-30 18:07:37 -0700

    added madlibs

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# youtuber = "Kylie Ying"
+
+# print(f"subscribe to {youtuber}")
+
+
+adj = input("Adjective: ")
+
+verb1 = input("verb 1: ")
+
+verb2 = input("verb 2: ")
+
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
